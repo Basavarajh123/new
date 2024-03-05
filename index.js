a1cf0957c4ddc66a9d3ecacc7afaadd9473c9741
Basavarajh123 <basavarajh556@gmail.com>
2024-03-05 21:27:26 +0530

    adding database

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 
 const express = require('express');
+const cors=require('cors');
+const {open}= require('sqlite');
+const sqlite3= require('sqlite3');
+const path = require('path');
+const dbPath = path.join(__dirname,"User.db");
+
+let database=null;
 
 
 
@@ -9,19 +16,64 @@ app.use(express.json());
 
 
 
+const inittializeDbAndServer=async()=>{
+    try{
+        database = await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        })
+    
+        app.listen(4000,()=>{
+            console.log('Listening at http://localhost:4000')
+        
+        })
+
+    }catch(error){
+        console.log(`DB Error :${error.message}`);
+        process.exit(1);
+    }
+   
+
+
+
+}
+
+inittializeDbAndServer();
+
 app.get('/',(req,res)=>{
 
     res.send('Hello Welcome to Tech World');
 })
 
 
-app.get('/login',(req,res)=>{
-    res.send('Wel Come Basavaraj')
+app.get('/users',async(req,res)=>{
+    const sqlQuery=`SELECT * FROM User`
+    const data = await database.all(sqlQuery);
+    res.send(data);
+    
 })
 
-app.listen(4000,()=>{
-    console.log('Listening at port http://localhost:4000');
+app.post("/signup",async(req,res)=>{
 
+    const{name,email,password}= req.body;
+
+    const sqlQuery=`INSERT INTO User(username,email,password)
+                                VALUES("${name}","${email}","${password}")
+    
+    `
+    await database.run(sqlQuery);
+    res.send('User Added Successfully');
 })
+
+app.post("/login", async(req,res)=>{
+    const {email,password}= req.body;
+    const sqlQuery=`SELECT * FROM User WHERE email="${email}" and password="${password}";`;
+    const data = await database.get(sqlQuery);
+    if (data!== undefined){
+        res.send('User Credentail are Correct');
+    }
+})
+
+
 
 module.exports= app;
---
+++ mysql.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE User(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--                     username VARCHAR(30) NOT NULL,
+--                     email VARCHAR(30) NOT NULL,
+--                     password VARCHAR(30) NOT NULL
+--                     )
+
+-- -- INSERT INTO User(username,email,password)
+--             VALUES("Basavaraj","basavarajh556@gmail.com","Basu123@"),
+--             ("Basavaraj","basavarajh556@gmail.com","Basu123@"),
+--             ("Basavaraj","basavarajh556@gmail.com","Basu123@"),
+--             ("Basavaraj","basavarajh556@gmail.com","Basu123@"),
+--             ("Basavaraj","basavarajh556@gmail.com","Basu123@");
+
+SELECT * FROM User
